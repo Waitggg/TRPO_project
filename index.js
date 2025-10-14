49d1d00d7c4c67a75a5663ab2b94eda2ac21b5f7
Waitggg <kirillkorolt2589@gmail.com>
2025-10-14 21:44:08 +0300

    feat: express and multer(Img upload now works)

--- index.js
+++ index.js
@@ -1,51 +1,98 @@
+const express = require('express');
+const multer = require('multer');
 const fs = require('fs');
-const http = require('http');
+const path = require('path');
+
+const app = express();
 const port = 3000;
 
 const haram = {
-	vak4: '4vak',
-	vak5: '5vak'
+    vak4: '4vak',
+    vak5: '5vak'
 }
 
-const requestHandler = (request, response) =>{
-	console.log(`URL: ${request.url}`);
-	switch (request.url)
-	{
-	case '/haram':
-		response.end(JSON.stringify(haram));
-		break;
-	case '/index.css':
-		fs.readFile('C:/\Users/\Admin/\git_html/\TRPO_project/\index.css', (err, data) => {
-        if (err) {
-          response.end('Error: ', err);
-        }else{
-        	response.end(data);
-        }});
-		break;
-	case '/game.js':
-		fs.readFile('C:/\Users/\Admin/\git_html/\TRPO_project/\game.js', (err, data) => {
-        if (err) {
-          response.end('Error: ', err);
-        }else{
-        	response.end(data);
-        }});
-		break;
-	default:
-		fs.readFile('C:/\Users/\Admin/\git_html/\TRPO_project/\index.html', (err, data) => {
-        if (err) {
-          response.end('Error: ', err);
-        }else{
-        	response.end(data);
-        }});
-		break;
-	}
-};
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        const uploadDir = 'uploads';
+        if (!fs.existsSync(uploadDir)) {
+            fs.mkdirSync(uploadDir);
+        }
+        cb(null, uploadDir);
+    },
+    filename: function (req, file, cb) {
+        const uniqueName = Date.now() + '-' + file.originalname;
+        cb(null, uniqueName);
+    }
+});
 
-const server = http.createServer(requestHandler);
+const upload = multer({ 
+    storage: storage,
+    fileFilter: function (req, file, cb) {
+        // Проверяем тип файла (опционально)
+        if (file.mimetype.startsWith('image/')) {
+            cb(null, true);
+        } else {
+            cb(new Error('Разрешены только изображения!'), false);
+        }
+    },
+    limits: {
+        fileSize: 20 * 1024 * 1024
+    }
+});
 
-server.listen(port, (err) => {
-	if(err){
-		return console.log('Error: ', err);
-	}
-	console.log(`server is listening on ${port}`);
+app.use(express.static('public'));
+
+app.get('/haram', (req, res) => {
+    res.json(haram);
+});
+
+app.get('/index.css', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.css'));
+});
+
+app.get('/game.js', (req, res) => {
+    res.sendFile(path.join(__dirname, 'game.js'));
+});
+
+app.post('/img', upload.single('file'), (req, res) => {
+    try {
+        if (!req.file) {
+            return res.status(400).send('Файл не был загружен');
+        }
+
+        res.json({
+            message: 'Файл успешно загружен!',
+            file: {
+                originalname: req.file.originalname,
+                filename: req.file.filename,
+                size: req.file.size,
+                url: `/uploads/${req.file.filename}`
+            }
+        });
+    } catch (error) {
+        console.error('Ошибка загрузки:', error);
+        res.status(500).send('Ошибка при загрузке файла');
+    }
+});
+
+app.use('/uploads', express.static('uploads'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.use((error, req, res, next) => {
+    if (error instanceof multer.MulterError) {
+        if (error.code === 'LIMIT_FILE_SIZE') {
+            return res.status(400).send('Файл слишком большой');
+        }
+    }
+    res.status(500).send('Что-то пошло не так');
+});
+
+app.listen(port, (err) => {
+    if (err) {
+        return console.log('Error: ', err);
+    }
+    console.log(`Express server is listening on ${port}`);
 });
